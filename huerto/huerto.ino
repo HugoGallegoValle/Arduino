e24efa7342c44e6c227cd37dcecbab680c2655a0
HYC <147177385+HugoGallegoValle@users.noreply.github.com>
2026-02-13 22:16:58 +0100

    Primera iteración del huerto

---
+++ huerto/huerto.ino
@@ -0,0 +1,59 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+int humedad_tierra;
+int temperatura;
+int humedad;
+int luminosidad;
+Adafruit_SSD1306 pantalla(128,64,&Wire,-1);
+DHT dht(12,DHT11);
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
+  if(!pantalla.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+  Serial.println("ERROR 404");
+  for(;;);
+}
+dht.begin();
+/*
+pantalla.clearDisplay();
+pantalla.setCursor(37,25);
+pantalla.setTextSize(2);
+pantalla.setTextColor(WHITE);
+pantalla.write("HUGO");
+pantalla.display();
+pantalla.drawRect(20,20,80,25,WHITE);
+pantalla.display();
+*/
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  pantalla.clearDisplay();
+  humedad_tierra=analogRead(A0);
+  temperatura=dht.readTemperature();
+  humedad=dht.readHumidity();
+  luminosidad=map(analogRead(A1),0,400,0,100);
+  Serial.println(luminosidad);
+  pantalla.setCursor(1,10);
+  pantalla.setTextSize(1);
+  pantalla.setTextColor(WHITE);
+  pantalla.write("Humedad:");
+  pantalla.print(humedad);
+  pantalla.print("%");
+  pantalla.setCursor(1,20);
+  pantalla.print("Temperatura:");
+  pantalla.print(temperatura);
+  pantalla.drawCircle(86,20,1,WHITE);
+  pantalla.setCursor(88,20);
+  pantalla.print("C");
+  pantalla.setCursor(1,30);
+  pantalla.print("Luminosidad:");
+  pantalla.print(luminosidad);
+  pantalla.print("%");
+  pantalla.display();
+  delay(1000);
+}
