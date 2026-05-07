cf8a99e5a8be9608e271edf30f7ea8147d73c243
HYC <147177385+HugoGallegoValle@users.noreply.github.com>
2026-05-07 16:25:10 +0200

    7/5/2026

---
+++ pir_telegram/pir_telegram.ino
@@ -0,0 +1,57 @@
+#include <ArduinoJson.h>
+#include <UniversalTelegramBot.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+const char* nombre_wifi = "DIGIFIBRA-2133";
+const char* contraseña = "A7TJXB3Q4A";
+const char* token = "8779274087:AAFsUdmRY06VRvewyflMP3cckJngghY8nAA";
+const char* id = "";
+WiFiClientSecure client;
+UniversalTelegramBot bot(token, client);
+bool movimiento;
+int pin_pir=27;
+void funcion(bool movimiento) { 
+  return movimiento=true;
+}
+void setup() {
+  pinMode(pin_pir, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(pin_pir),funcion(bool movimiento), RISING);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(nombre_wifi, contraseña);
+  Serial.begin(115200);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);  // Certificado Raíz para api.telegram.org - Almacenado en la librería
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println(WiFi.localIP());
+  bot.sendMessage(id, "Hola", "");
+  pinMode(25, OUTPUT);
+}
+void mensajes(int numeroMensajesNuevos) {
+  for (int i = 0; i < numeroMensajesNuevos; i++) {
+    String chat_id = String(bot.messages[i].chat_id);  //Guarda la ID del ususraio que escriba en la string chat_id
+    if (chat_id != id) {                               //Comprueba si el usuario somos nosotros
+      bot.sendMessage(id, "Usuario no autorizado. ACCESO DENEGADO", "");
+      continue;
+    }
+    String mensaje = bot.messages[i].text;  //Guarda el mensaje i(último) en la variable mensaje
+    if (mensaje == "/zumbador_on") {
+      digitalWrite(25, HIGH);
+      bot.sendMessage(id, "Encendiendo el zumbador", "");
+    } else if (mensaje == "/zumbador_off") {
+      digitalWrite(25, LOW);
+      bot.sendMessage(id, "Apagando el zumabdor", "");
+    } else {
+      bot.sendMessage(id, "ERROR al interpretar el mensaje", "");
+    }
+  }
+}
+void loop() {
+  int numeroMensajesNuevos = bot.getUpdates(bot.last_message_received + 1);
+  mensajes(numeroMensajesNuevos);
+  if (movimiento) {
+    bot.sendMessage(id, "Hay gente en tu casa", "");
+  }
+  movimiento=false;
+}
